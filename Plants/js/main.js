30297e10849ff1b8981018d2367732629e950ef9
whcho888 <whcho888@gmail.com>
2014-09-17 00:28:11 +0900

    장소 배열 생성 및 카테고리 변경

--- Plants/js/main.js
+++ Plants/js/main.js
@@ -1,3 +1,67 @@
+var dst_eng = [
+		"atm",
+		"bakery",
+		"bank",
+		"bar",
+		"bus_station",
+		"cafe",
+		"clothing_store",
+		"convenience_store",
+		"department_store",
+		"florist",
+		"food",
+		"gas_station",
+		"grocery_or_supermarket",
+		"hair_care",
+		"hospital",
+		"library",
+		"liquor_store",
+		"movie_theater",
+		"museum",
+		"night_club",
+		"park",
+		"parking",
+		"pharmacy",
+		"police",
+		"restaurant",
+		"school",
+		"shoe_store",
+		"shopping_mall",
+		"spa",
+		"store",
+		"subway_station",
+		"university",
+		"zoo"
+]
+
+var dst_kor = [
+               "현금지급기",
+               "베이커리",
+               "은행",
+               "바(bar)",
+               "버스정류장",
+               "카페",
+               "옷가게",
+               "편의점",
+               "백화점",
+               "화원",
+               "음식점",
+               "주유소",
+               "슈퍼마켓",
+               "미용실",
+               "병원",
+               "도서관",
+               "숩 판매점",
+               "영화관",
+               "박물관",
+               "클럽",
+               "공원",
+               "주차장",
+               "약국",
+               "경찰서"         
+ ]
+
+
 var backEventListener = null;
 
 var unregister = function() {
@@ -7,7 +71,7 @@ var unregister = function() {
      window.tizen.application.getCurrentApplication().exit();
  }
 }
-    document.addListener('tizenhwkey', function(button) {
+   /* document.addListener('tizenhwkey', function(button) {
     	switch(e.keyName)
     	        {
     	            case 'back':
@@ -26,8 +90,17 @@ var unregister = function() {
     	                // TODO: write a code for pressing menu button
     	                break;
     	        }
-    	});
- 
+    	});*/
+    $(window).on("tizenhwkey", function(ev) 
+    		{
+    		   if (ev.originalEvent.keyName === "back") 
+    		   {
+    			   
+    			   window.history.back()
+    		      /* Call window.history.back() to go to previous browser window */
+    		      /* or add script to add another behavior */
+    		   }
+    		})
     
 //Initialize function
 var init = function () {
@@ -35,23 +108,10 @@ var init = function () {
     if ( backEventListener !== null ) {
         return;
     }
-    
     // TODO:: Do your initialization job
     console.log("init() called");
   
-    $(window).on("tizenhwkey", function(ev) 
-    		{
-    		   if (ev.originalEvent.keyName === "back") 
-    		   {
-    			   
-    			   $.mobile.back();
-    		      /* Call window.history.back() to go to previous browser window */
-    		      /* or add script to add another behavior */
-    		   }
-    		})
-   
- 
-    var backEvent = function(e) {
+   var backEvent = function(e) {
        if ( e.keyName == "back" ) {
           try {
                 if ( $.mobile.urlHistory.activeIndex <= 0 ) {
