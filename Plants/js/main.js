c3bf85b8caee02d434e7cc97718e689e88917177
whcho888 <whcho888@gmail.com>
2014-09-16 23:03:52 +0900

    back key(not complete)

--- Plants/js/main.js
+++ Plants/js/main.js
@@ -1,42 +1,12 @@
-//var backEventListener = null;
-document.addListener('tizenhwkey', function(button) {
-	switch(e.keyName)
-	        {
-	            case 'back':
-	            	$.mobile.back();
-	                switch($.mobile.activePage)
-	                {
-	                    case 'one': // use your first page or another page where the application should close if the use press back
-	                    	$.mobile.back();
-	                    		tizen.application.getCurrentApplication().exit();
-	                        break;
-	                    default: // if no case available, the back button returns back to previous page
-	                        $.mobile.back();
-	                }
-	                break;
-	            case 'menu':
-	                // TODO: write a code for pressing menu button
-	                break;
-	        }
-	});
-//var unregister = function() {
-//    if ( backEventListener !== null ) {
-//       document.removeEventListener( 'tizenhwkey', backEventListener );
-//       backEventListener = null;
- //     window.tizen.application.getCurrentApplication().exit();
- //  }
-//}
+var backEventListener = null;
 
-//Initialize function
-var init = function () {
-    // register once
+var unregister = function() {
     if ( backEventListener !== null ) {
-        return;
-    }
-    
-    // TODO:: Do your initialization job
-    console.log("init() called");
-    var backEvent = function(e) {
+       document.removeEventListener( 'tizenhwkey', backEventListener );
+       backEventListener = null;
+     window.tizen.application.getCurrentApplication().exit();
+ }
+}
     document.addListener('tizenhwkey', function(button) {
     	switch(e.keyName)
     	        {
@@ -58,23 +28,46 @@ var init = function () {
     	        }
     	});
  
-    //var backEvent = function(e) {
-    //    if ( e.keyName == "back" ) {
-      //      try {
-        //        if ( $.mobile.urlHistory.activeIndex <= 0 ) {
-          //          // if first page, terminate app
-            //        unregister();
-          //      } else {
+    
+//Initialize function
+var init = function () {
+    // register once
+    if ( backEventListener !== null ) {
+        return;
+    }
+    
+    // TODO:: Do your initialization job
+    console.log("init() called");
+  
+    $(window).on("tizenhwkey", function(ev) 
+    		{
+    		   if (ev.originalEvent.keyName === "back") 
+    		   {
+    			   
+    			   $.mobile.back();
+    		      /* Call window.history.back() to go to previous browser window */
+    		      /* or add script to add another behavior */
+    		   }
+    		})
+   
+ 
+    var backEvent = function(e) {
+       if ( e.keyName == "back" ) {
+          try {
+                if ( $.mobile.urlHistory.activeIndex <= 0 ) {
+                    // if first page, terminate app
+                   unregister();
+                } else {
                     // move previous page
-            //        $.mobile.urlHistory.activeIndex -= 1;
-              //      $.mobile.urlHistory.clearForward();
-                //    window.history.back();
-  //              }
-    //        } catch( ex ) {
-      //          unregister();
-     //       }
-      //  }
-   // }
+                    $.mobile.urlHistory.activeIndex -= 1;
+                    $.mobile.urlHistory.clearForward();
+                    window.history.back();
+              }
+           } catch( ex ) {
+                unregister();
+           }
+        }
+    }
     
     // add eventListener for tizenhwkey (Back Button)
     document.addEventListener( 'tizenhwkey', backEvent );
