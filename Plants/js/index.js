1e7a972804863d02cdd54ac612d8e20755381a4e
whcho888 <whcho888@gmail.com>
2014-09-17 02:02:00 +0900

    send info to server

--- Plants/js/index.js
+++ Plants/js/index.js
@@ -34,3 +34,101 @@ $(document).ready(function(){
 
 	google.maps.event.addDomListener(window, 'load', initialize);
 });
+
+var dst_eng = [
+       		"atm",
+       		"bakery",
+       		"bank",
+       		"bar",
+       		"bus_station",
+       		"cafe",
+       		"clothing_store",
+       		"convenience_store",
+       		"department_store",
+       		"florist",
+       		"food",
+       		"gas_station",
+       		"grocery_or_supermarket",
+       		"hair_care",
+       		"hospital",
+       		"library",
+       		"liquor_store",
+       		"movie_theater",
+       		"museum",
+       		"night_club",
+       		"park",
+       		"parking",
+       		"pharmacy",
+       		"police",
+       		"restaurant",
+       		"school",
+       		"shoe_store",
+       		"shopping_mall",
+       		"spa",
+       		"store",
+       		"subway_station",
+       		"university",
+       		"zoo"
+       ]
+
+       var dst_kor = [
+                      "현금지급기",
+                      "베이커리",
+                      "은행",
+                      "바(bar)",
+                      "버스정류장",
+                      "카페",
+                      "옷가게",
+                      "편의점",
+                      "백화점",
+                      "화원",
+                      "음식점",
+                      "주유소",
+                      "슈퍼마켓",
+                      "미용실",
+                      "병원",
+                      "도서관",
+                      "술 판매점",
+                      "영화관",
+                      "박물관",
+                      "클럽",
+                      "공원",
+                      "주차장",
+                      "약국",
+                      "경찰서"         
+        ]
+
+var send_msg;
+
+$('#searchList')
+.click(
+      function(e) {
+         var name = e.target.text;
+         	
+         for(var idx = 0; idx < dst_eng.length; idx++)
+         {
+        	 	if(dst_eng[idx] == name)
+        	 		break;
+         }
+         send_msg = dst_eng[idx];
+       	}
+      )
+         
+var url = 'http://210.118.74.119:8080//hackathon/getSearchList.do?keyword='+keyword;
+$.ajax({
+	url : url,
+	type : 'get',
+	data : { category : send_msg } ,
+	dataType : 'json',
+	success : function(result) {
+		searchList = result;
+		 for(var idx=0; idx<result.length; idx++) {
+			 $('#searchList').append('<a href="#" class="list-group-item">'+result[idx].name+'</a>');
+		 }
+	},
+	error : function(e) {
+		console.log(e.responseText);
+	}
+});   
+
+
